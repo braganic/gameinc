67b307ad383e6c35a3765f7125ad1c1adc037905
Xavier Nou <xavier.nou1@gmail.com>
2018-12-04 16:11:35 -0300

    footer en todos los archivos con require_once

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer>
+  <div class="footer-container">
+    <div class="footer-column1">
+      <div class="social-media">
+      <label for=""> Seguinos en <a href="#"><i class="fab fa-facebook-f"></i></a><a href="#"><i class="fab fa-instagram"></i></a></label>
+      </div>
+    </div>
+    <div class="footer-column2">
+      <h3> NOSOTROS </h3>
+       <ul>
+         <li>Quienes Somos</li>
+         <li>Contacto</li>
+       </ul>
+    </div>
+    <div class="footer-column3">
+      <h3> LEGALES </h3>
+       <ul>
+         <li>Ayuda</li>
+         <li>Politica de Privacidad</li>
+         <li>Terminos y condiciones</li>
+       </ul>
+    </div>
+    <div class="footer-column4">
+      <h3> COMPRA 100% SEGURA </h3>
+       <ul>
+        <li>Nuestro sitio posee toda la seguridad que necesitas para realizar tu compra</li>
+        <li><i class="fas fa-check-circle"></i><i class="fas fa-user-check"></i></li>
+      </ul>
+    </div>
+  </div>
+  <div class="rights">
+    <div class="logo">
+      <a href="#">GAME<span style="color:#FC1B1A; margin-left:2px">INC</span></a>
+    </div>
+    <p> ©Todos los derechos reservados. </p>
+  </div>
+</footer>
