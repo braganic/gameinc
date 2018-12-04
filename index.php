d72a2c09e092910799873d2e16c23150ccc0abdf
Xavier Nou <xavier.nou1@gmail.com>
2018-12-04 17:00:15 -0300

    index.php con footer por require

--- index.php
+++ index.php
@@ -169,43 +169,9 @@
       </section>
       <span class="background"></span>
     </main>
-    <footer>
-      <div class="footer-container">
-        <div class="footer-column1">
-          <div class="social-media">
-          <label for=""> Seguinos en <a href="#"><i class="fab fa-facebook-f"></i></a><a href="#"><i class="fab fa-instagram"></i></a></label>
-          </div>
-        </div>
-        <div class="footer-column2">
-          <h3> NOSOTROS </h3>
-           <ul>
-             <li>Quienes Somos</li>
-             <li>Contacto</li>
-           </ul>
-        </div>
-        <div class="footer-column3">
-          <h3> LEGALES </h3>
-           <ul>
-             <li>Ayuda</li>
-             <li>Politica de Privacidad</li>
-             <li>Terminos y condiciones</li>
-           </ul>
-        </div>
-        <div class="footer-column4">
-          <h3> COMPRA 100% SEGURA </h3>
-           <ul>
-            <li>Nuestro sitio posee toda la seguridad que necesitas para realizar tu compra</li>
-            <li><i class="fas fa-check-circle"></i><i class="fas fa-user-check"></i></li>
-          </ul>
-        </div>
-      </div>
-      <div class="rights">
-        <div class="logo">
-          <a href="#">GAME<span style="color:#FC1B1A; margin-left:2px">INC</span></a>
-        </div>
-        <p> ©Todos los derechos reservados. </p>
-      </div>
-    </footer>
+    <?php
+      require_once("components/footer.php");
+     ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
