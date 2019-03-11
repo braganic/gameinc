31d7797d786ea6f64b971c8bce536055a28e76be
Unknown <xavier.nou1@gmail.com>
2019-03-11 19:08:49 -0300

    search css

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,28 +10,37 @@
   <table class="table table-striped">
     <thead>
       <tr>
-        <th scope="col">Imagen</th>
+        <th style="width:25%;" scope="col">Imagen</th>
         <th scope="col">Producto</th>
+        <th scope="col">Precio</th>
+        <th scope="col">Link</th>
       </tr>
     </thead>
     @foreach ($products as $product)
-    <tr>
-      <td>
+    <tr style="height:180px;">
+      <td valign="middle" >
         <a href="/products/{{$product->id}}">
-          <img src="/storage/{{$product->foto}}" alt="" height="150" width="150">
+          <img src="/storage/{{$product->foto}}" alt="" height="180" width="140">
         </a>
       </td>
-      <td>
+      <td valign="middle" style="height:180px;">
         <a href="/products/{{$product->id}}" style="font-size: 1.5rem;">
           {{$product->name}}
         </a>
+
+
+      </td>
+      <td valign="middle" style="height:180px;">
         <div class="">
           $ {{$product->price}}
         </div>
-        <div class="">
-          <a href="/products/{{$product->id}}" class="btn btn-danger">Ver detalle</a>
-        </div>
-      </td>
+        </td>
+        <td>
+          <div class="">
+            <a href="/products/{{$product->id}}" class="btn btn-danger">Ver detalle</a>
+          </div></td>
+
+
     </tr>
 @endforeach
 
