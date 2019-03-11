6e63e173507c1fadb172a5dcfeab88b006da79e3
Nicolas Braga <braganicolas1@gmail.com>
2019-03-11 17:48:39 -0300

    pagina de productos

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,28 +5,46 @@
   @endsection
 
   @section("principal")
-<h1>Productos</h1>
-<div class="container">
-<p>Filtro por categoría</p>
-<form action="/category" method="get">
-  {{csrf_field()}}
-  <select class="" name="category">
-    @foreach ($categories as $category)
-      <option value="{{$category->id}}">{{$category->name}}</option>
+  <div class="container">
+    <h1>Productos</h1>
+    <div class="container">
+    <p>Filtro por categoría</p>
+    <form action="/category" method="get">
+      {{csrf_field()}}
+      <select class="" name="category">
+        @foreach ($categories as $category)
+          <option value="{{$category->id}}">{{$category->name}}</option>
+        @endforeach
+      </select>
+      <button type="submit" name="button">Aplicar</button>
+    </form>
+    </div>
+
+
+    @foreach ($products as $product)
+
+        <div class="card" style="width: 18rem; margin: 35px; display: inline-block;">
+      <img class="card-img-top" src="/storage/{{$product->foto}}" alt="{{$product->name}}" width="223" height="281">
+      <div class="card-body">
+        <h5 class="card-title">{{$product->name}}</h5>
+        <p class="card-text">Precio: ${{$product->price}}</p>
+        <a href="/products/{{$product->id}}" class="btn btn-danger">Ver detalle</a>
+      </div>
+    </div>
     @endforeach
-  </select>
-  <button type="submit" name="button">Aplicar</button>
-</form>
 
-</div>
 
-<ul>
-  @foreach ($products as $product)
-    <li>
-      <a href="/products/{{$product->id}}">
-        {{$product->name}}
-      </a>
-    </li>
-  @endforeach
-</ul>
+    <!-- <ul>
+      @foreach ($products as $product)
+        <li>
+          <a href="/products/{{$product->id}}">
+            {{$product->name}}
+          </a>
+        </li>
+      @endforeach
+    </ul> -->
+
+
+  </div>
+
   @endsection
