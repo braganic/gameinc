f0a14b588a1b1fdfe8df77e2baa46dd181e0bdaa
Nicolas Braga <braganicolas1@gmail.com>
2019-03-11 19:10:57 -0300

    modal

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -12,7 +12,27 @@
 
   <div class="product-left">
     <div class="img-container">
-      <img src="/storage/{{$product->foto}}" alt="">
+
+            <!-- Button trigger modal -->
+      <a href="/" class="" data-toggle="modal" data-target="#exampleModal">a
+        <img src="/storage/{{$product->foto}}" alt="">
+      </a>
+
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img src="/storage/{{$product->foto}}" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
