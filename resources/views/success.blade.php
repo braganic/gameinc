ceec63158289fbf1d77867e30acd6549f12e9931
Unknown <xavier.nou1@gmail.com>
2019-03-12 12:16:05 -0300

    success page con vinculo a mas productos

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,1 +1,19 @@
-Compra realizada con éxito
+@extends('plantilla')
+@section('titulo')
+  Compra Finalizada
+@endsection
+@section('principal')
+  <div class="container" style="background-color:rgba(39, 244, 13, 0.4); border: 1px solid rgb(34, 233, 84); border-radius:5px; margin: 0 auto;
+  margin-top: 60px;">
+    <div class="" style="">
+      <ps style=""> <strong>Felicitaciones por tu compra! Segui buscando, tenemos ofertas que no te podes perder!</p>
+    </div>
+  </div>
+  <div class="container" style="background-color:rgba(215, 23, 23, 0.5); border: 1px solid rgb(230, 68, 68); border-radius:5px; margin: 0 auto;
+  margin-top: 40px;">
+    <div class="" style="">
+      <ps style=""> <strong>Si te llevas otro producto ahora mismo, te ofrecemos un 15% de descuento en el mismo!</p>
+    </div>
+  </div>
+  <a href="/products" class="btn btn-info" style="margin-top:40px; margin-left: 35%; width: 30%;">Ver más</a>
+@endsection
