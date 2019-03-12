9f0e24f1f0422f153e509ab02f87b8f19570086c
Nicolas Braga <braganicolas1@gmail.com>
2019-03-12 11:04:13 -0300

    fix eliminar producto

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -54,24 +54,31 @@
       </li>
     </ul>
 
-  @if (Auth::check())
-    @if (auth()->user()->type == 2)
-     <form class="" action="/deleteProduct" method="post">
-     {{ csrf_field() }}
-     <input type="hidden" name="idProduct" value="{{$product->id}}">
-     <button type="submit" name="button" class="btn btn-danger">Eliminar producto</button>
-    @endif
-  @endif  
 
-      @if (!$cart->contains($product))
-       <form action="/addToCart/{{$product->id}}" method="post">
-        {{csrf_field()}}
-        <button type="Submit" name="button" class="btn btn-success">Añadir al carrito</button>
-      </form>
-      @else
-      <p>El producto ya fue añadido al carrito</p>
-      <a href="/cart" class="btn btn-danger">Ver carrito</a>
-      @endif
+      
+        @if (!$cart->contains($product))
+         <form action="/addToCart/{{$product->id}}" method="post">
+          {{csrf_field()}}
+          <button type="Submit" name="button" class="btn btn-success">Añadir al carrito</button>
+        </form>
+        @else
+        <p>El producto ya fue añadido al carrito</p>
+        <a href="/cart" class="btn btn-danger">Ver carrito</a>
+        @endif
+
+
+
+
+        @if (Auth::check())
+          @if (auth()->user()->type == 2)
+           <form class="" action="/deleteProduct" method="post">
+           {{ csrf_field() }}
+           <input type="hidden" name="idProduct" value="{{$product->id}}">
+           <button type="submit" name="button" class="btn btn-danger">Eliminar producto</button>
+          @endif
+        @endif
+
+
   </div>
 </div>
 
