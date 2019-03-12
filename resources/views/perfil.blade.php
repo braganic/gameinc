67b015d62ff6a9c37c1410c0c5ee3345d0b6fa70
Unknown <braganicolas1@gmail.com>
2019-03-12 00:40:34 -0300

    perfil usuario

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -5,22 +5,34 @@
   @endsection
 
   @section("principal")
-<h1>Bienvenido a tu perfil, {{$usuario->name}}</h1>
-<ul>
-  <li>
-    Tu email:{{$usuario->email}}
-  </li>
-  <li>
-  {{$usuario->name}}
-  </li>
-  <li>
-    {{$usuario->name}}
-  </li>
-  <li>
-    {{$usuario->name}}
-  </li>
+
+    <div class="container">
+      <h1>Bienvenido a tu perfil, {{$usuario->name}}</h1>
+      <img src="/storage/{{$usuario->avatar}}" alt="" width="100" height="100">
+      <table class="table">
+        <tr>
+          <th>Nombre:</th>
+          <td>{{$usuario->name}}</td>
+        </tr>
+        <tr>
+          <th>Email:</th>
+          <td>{{$usuario->email}}</td>
+        </tr>
+        <tr>
+          <th>Pais:</th>
+          <td>{{$usuario->country}}</td>
+        </tr>
+        <tr>
+          <th>Tipo de cuenta:</th>
+          @if ($usuario->type == 1)
+          <td>Cliente</td>
+          @else
+          <td>Admin</td>
+          @endif
+        </tr>
+      </table>
+      <p></p>
+    </div>
 
 
-
-</ul>
 @endsection
